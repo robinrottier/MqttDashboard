d224cc9fa5ecd3d09b4ee818ac4690843c1b261f
Robin Rottier <robin@rottier.uk>
2026-02-10 20:59:21 +0000

    Add styling for diagram boxes to use mud compoents and handles to still work

---
+++ BlazorApp1/Models/MudNodeModel.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.Diagrams.Core.Models;
+using Blazor.Diagrams.Core.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Models
+{
+    public class MudNodeModel : NodeModel
+    {
+        public MudNodeModel(Point? position = null) : base(position)
+        {
+        }
+
+    }
+}
